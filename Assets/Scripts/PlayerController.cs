8db4e34a9984bd0f2af01662b65d5e0271c64af5
DaHee Kim <34751044+dahkimr@users.noreply.github.com>
2024-11-13 00:37:50 -0500

    player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 6;
+    private Rigidbody rigidBody;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.linearVelocity = new Vector3(moveInput.x * moveSpeed, 0, moveInput.y * moveSpeed);
+    }
+
+    void OnMove(InputValue value) {
+        Debug.Log("onMove was called");
+        moveInput = value.Get<Vector2>();
+    }
+}
